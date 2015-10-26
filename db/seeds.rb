c834365e9f5fdc79ec941e0842f1688058528143
Steven Lee-Kramer <sleekramer@gmail.com>
2015-10-26 18:27:28 -0400

    Complete assignment-30, Add unique post and comment with find_or_create_by method in db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,11 @@ end
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+
+ Post.find_or_create_by(title: "Unique Post") do |post|
+   post.body = "This post should only be added to the database one time."
+ end
+ Comment.find_or_create_by(
+    body: "This is a unique comment. It will only be added once",
+    post: Post.find(101)
+  )
