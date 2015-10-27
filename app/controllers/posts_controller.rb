f17067fbe8931ba9cf7e1ae4bd4bc4b193356461
Steven Lee-Kramer <sleekramer@gmail.com>
2015-10-26 23:06:52 -0400

    Complete assignment-31, Add feature in index method of PostsController to title every 5th post as 'SPAM'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each do |post|
+      post.title = "SPAM" if post.id % 5 == 0
+    end
   end
 
   def show
