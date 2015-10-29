3438b5bcba60f093b861b85018fc34dfa5508334
Steven Lee-Kramer <sleekramer@gmail.com>
2015-10-29 15:26:33 -0400

    Update files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
 # Shoulda tests for name
   it {should validate_presence_of(:name)}
   it {should validate_length_of(:name).is_at_least(1)}
+
 # Shoulda tests for email
   it {should validate_presence_of(:email)}
   it {should validate_uniqueness_of(:email)}
