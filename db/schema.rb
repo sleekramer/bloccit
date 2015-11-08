3336bffbbe26c88b9c3aa88883efa250ce70c601
Steven Lee-Kramer <sleekramer@gmail.com>
2015-11-07 22:16:21 -0500

    Complete checkpoint-48, Build read-only api that allows all users to GET Topics and only admin users to GET users, Add generate_auth_token method to User and auth_token column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104224033) do
+ActiveRecord::Schema.define(version: 20151107162205) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 20151104224033) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
